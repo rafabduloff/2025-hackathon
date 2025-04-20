4f5acada18bbe245241b6df42f9e9b1bed718f1b
rafabduloff <136557397+rafabduloff@users.noreply.github.com>
2025-04-20 13:13:43 +0500

    Update MarvelScene.tsx

--- src/components/ThreeScene/MarvelScene.tsx
+++ src/components/ThreeScene/MarvelScene.tsx
@@ -120,7 +120,7 @@ const Scene: React.FC<{ selectedCharacter: number; onCharacterSelect: (index: nu
   const characters = [
     {
       name: 'Iron Man',
-      model: '/models/iron_man_mark_85.glb',
+      model: 'https://ucarecdn.com/1b8b4216-700d-4607-8177-a583b985676b.glb',
       position: [0, 0, 0] as [number, number, number],
     },
     {
